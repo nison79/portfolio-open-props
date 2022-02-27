3aea3eb4c8f2ce735de0eedb4214c2bc1b4a2899
George Nikoglou <georgenison@gmail.com>
2022-02-27 14:58:15 +0200

    playing

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import "open-props/style";
 import "open-props/normalize";
 
-import confetti from "canvas-confetti";
+// import confetti from "canvas-confetti";
 
-confetti.create(null, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+// confetti.create(null, {
+//   resize: true,
+//   useWorker: true,
+// })({ particleCount: 200, spread: 200 });
 
-console?.log("ready.");
+// console?.log("ready.");
 
 var tl = gsap.timeline({ repeat: 0, repeatDelay: 1 });
 tl.to("#hello", { opacity: 1, duration: 3 });
 // tl.to("#hello", { x: 0, duration: 3 });
 // tl.to("#hello", { scale: 3, duration: 1.1 });
-tl.to("#hello", { x: -1000, duration: 6, ease: "power4" });
+// tl.to("#hello", { x: -1000, duration: 6, ease: "power4" });
 // tl.to("#hello", { opacity: 0, duration: 1 });
 
 var tlName = gsap.timeline({ repeat: 0, repeatDelay: 1 });
@@ -22,3 +22,15 @@ tlName.to("#name", { scale: 1, duration: 2 });
 // tlName.to("#name", { x: 0, duration: 3 });
 // tlName.to("#name", { scale: 3, duration: 0.5 });
 // tlName.to("#name", { x: -1000, duration: 1 });
+
+var tlName = gsap.timeline({ defaults: { duration: 3 } });
+tlName
+  .to("#angular", { scale: 1.5, duration: 1.5, ease: "power1" })
+  .to("#ionic", { scale: 1.2, duration: 2, ease: "power2" });
+
+// var tlName = gsap.timeline({ repeat: 23, repeatDelay: 1.2 });
+// tlName.to("#ionic", { scale: 1.3, duration: 1, ease: "bounce" });
+
+var tlName = gsap.timeline({ repeat: 2323, repeatDelay: 1 });
+
+tlName.to("#dot", { opacity: 0.5, duration: 0.1 });
